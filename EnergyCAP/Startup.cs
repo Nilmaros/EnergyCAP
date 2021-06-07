57161e26a90025de83b67cd23537fc695755cdb3
Mitchell <mitchell.hernandezbroadhurst@willistowerswatson.com>
2021-06-07 10:12:56 +0100

    Cleanup

--- EnergyCAP/Startup.cs
+++ EnergyCAP/Startup.cs
@@ -2,23 +2,13 @@ using EnergyCAP.Areas.Identity;
 using EnergyCAP.Interfaces;
 using EnergyCAP.Data;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.OAuth;
-using System.Security.Claims;
 
 namespace EnergyCAP
 {
