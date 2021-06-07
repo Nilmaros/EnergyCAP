789ff65c78ba18880fff8f4334bf5c29cccc33c0
Mitchell <mitchell.hernandezbroadhurst@willistowerswatson.com>
2021-06-07 09:00:45 +0100

    Adding new license column

---
+++ EnergyCAP/Data/Migrations/20210607080010_AddLicenseColumn.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EnergyCAP.Data.Migrations
+{
+    public partial class AddLicenseColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "License",
+                table: "Repos",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "License",
+                table: "Repos");
+        }
+    }
+}
