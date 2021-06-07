b546715e0e045e3e235cf4c50204409c97fdcc39
Mitchell <mitchell.hernandezbroadhurst@willistowerswatson.com>
2021-06-07 09:55:03 +0100

    RepoService adapting to code.

--- EnergyCAP/Data/RepoService.cs
+++ EnergyCAP/Data/RepoService.cs
@@ -1,65 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using EnergyCAP.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
 
 namespace EnergyCAP.Data
 {
     public class RepoService
     {
-        #region Property  
         private readonly ApplicationDbContext _dbContext;
-        #endregion
+        private readonly HttpClient _httpClient;
 
-        #region Constructor  
-        public RepoService(ApplicationDbContext ApplicationDbContext)
+        public RepoService(ApplicationDbContext ApplicationDbContext, HttpClient HttpClient)
         {
             _dbContext = ApplicationDbContext;
+            _httpClient = HttpClient;
         }
-        #endregion
 
-        #region Get List of Repos
-        public async Task<List<Repo>> GetAllReposAsyn()
+        /// <summary>
+        /// Insert Repo
+        /// </summary>
+        /// <param name="repo"></param>
+        /// <returns></returns>
+        public async Task<bool> InsertRepoAsync(Repo repo)
         {
-            return await _dbContext.Repos.ToListAsync();
-        }
-        #endregion
-
-        #region Insert Repo
-        public async Task<bool> InsertRepoAsync(Repo employee)
-        {
-            await _dbContext.Repos.AddAsync(employee);
+            await _dbContext.Repos.AddAsync(repo);
             await _dbContext.SaveChangesAsync();
             return true;
         }
-        #endregion
 
-        #region Get Repo by UserNameReposId
-        public async Task<List<Repo>> GetListReposAsync(int UserNameReposId)
+        /// <summary>
+        /// Get list of Repos from database
+        /// </summary>
+        /// <param name="username">Github username</param>
+        /// <returns></returns>
+        public async Task<List<Repo>> GetListReposAsync(string username)
         {
-            List<Repo> repos = await _dbContext.Repos.Where(c => c.Id.Equals(UserNameReposId)).ToListAsync();
-            return repos;
+            return await _dbContext.Repos.Where(c => c.Username.Equals(username)).ToListAsync();
         }
-        #endregion
 
-        #region Update Repo  
-        public async Task<bool> UpdateRepoAsync(Repo repo)
+        /// <summary>
+        /// Delete repo from database
+        /// </summary>
+        /// <param name="username">Github username</param>
+        /// <returns></returns>
+        public async Task<bool> DeleteRepoAsync(string username)
         {
-            _dbContext.Repos.Update(repo);
+            var repos = await _dbContext.Repos.Where(c => c.Username.Equals(username)).ToListAsync();
+            foreach (var repo in repos)
+            {
+                _dbContext.Remove(repo);
+            }
+
             await _dbContext.SaveChangesAsync();
+
             return true;
         }
-        #endregion
 
-        #region Delete Repo
-        public async Task<bool> DeleteRepoAsync(Repo repo)
+        /// <summary>
+        /// Get list of starred repos from Github
+        /// </summary>
+        /// <param name="username">Github username</param>
+        /// <returns></returns>
+        public async Task<List<Repo>> GetListStarredReposFromGithubAsync(string username)
         {
-            _dbContext.Remove(repo);
-            await _dbContext.SaveChangesAsync();
-            return true;
+            var header = new ProductInfoHeaderValue("User-Agent", "request");
+            _httpClient.DefaultRequestHeaders.UserAgent.Add(header);
+            var response = await _httpClient.GetAsync("https://api.github.com/users/" + username + "/starred");
+
+            var responseStream = await response.Content.ReadAsStringAsync();
+            dynamic responseObjects = JsonConvert.DeserializeObject(responseStream);
+            List<Repo> repList = new List<Repo>();
+            foreach (var responseItem in responseObjects)
+            {
+                Repo repo = new Repo();
+                repo.Name = responseItem.name;
+                repo.Description = responseItem.description;
+                repo.License = "";
+                var a = responseItem.license.GetType();
+
+                if (responseItem.license.GetType().Name == "JObject")
+                {
+                    repo.License = responseItem.license.key;
+                }
+
+                repo.Username = username;
+                repList.Add(repo);
+                await InsertRepoAsync(repo);
+            }
+            
+            return repList;
         }
-        #endregion
     }
 }
